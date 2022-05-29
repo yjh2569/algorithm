95cd54d815baea33f4290a7f7efd3e3317ed8146
yjh2569 <70595250+yjh2569@users.noreply.github.com>
2022-05-29 13:43:24 +0900

    BOJ 11058 크리보드
    * 다이나믹 프로그래밍을 이용하면 해결할 수 있는 문제입니다.
    * N의 값에 따라 어떤 길이의 문자열을 복사할지가 확연히 달라질 수 있기 때문에 주의해야 합니다.

--- algorithm/src/dp/Main_11058_크리보드.java
+++ algorithm/src/dp/Main_11058_크리보드.java
@@ -7,12 +7,17 @@ import java.io.InputStreamReader;
 public class Main_11058_크리보드 {
 	
 	public static void main(String[] args) throws IOException {
+		// 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		long[] dp = new long[N+1];
+		// 초기값
 		for (int i = 1; i <= N; i++) {
 			dp[i] = i;
 		}
+		// 다이나믹 프로그래밍을 적용하나, dp[i]를 계산할 때 dp[i-1]과 dp[i-3]만 가지고 바로 dp[i]를 계산할 수는 없다.
+		// 어떤 문자열을 복사하는지에 따라 당장은 dp[i]가 작을수는 있어도, dp[i+1]만 되어도 확연히 차이가 날 수 있기 때문이다.
+		// 따라서 O(N^2) 알고리즘으로 이전에 나왔던 가능한 모든 문자열을 복사하는 것을 시도해 보면서 dp[i]를 계산한다.
 		for (int i = 4; i <= N; i++) {
 			long temp = dp[i-3]*2;
 			for (int j = i; j <= N; j++) {
