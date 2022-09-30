8e98e438a35fb9857b3cd2fe1525bb35e35989bf
yjh2569 <70595250+yjh2569@users.noreply.github.com>
2022-09-30 20:28:53 +0900

    BOJ 1106 호텔
    * dynamic programming을 활용하면 해결할 수 있는 문제입니다.

--- algorithm/src/dp/Main_1106_호텔.java
+++ algorithm/src/dp/Main_1106_호텔.java
@@ -20,11 +20,16 @@ public class Main_1106_호텔 {
 			costs[i][0] = Integer.parseInt(st.nextToken());
 			costs[i][1] = Integer.parseInt(st.nextToken());
 		}
+		// dynamic programming을 활용한다.
+		// dp[i]는 적어도 i명을 데리고 오기 위해 필요한 최소 금액을 의미한다.
+		// 입력에 따라 i명 초과로 데리고 왔을 때에도 최소 금액이 발생할 수 있어 2*C명까지 데리고 오는 경우까지 고려했다.
 		int[] dp = new int[2*C+1];
+		// 초기화
 		for (int i = 0; i <= N; i++) {
 			Arrays.fill(dp, Integer.MAX_VALUE/10);
 		}
 		dp[0] = 0;
+		// i명 데리고 오기 위해 필요한 최소 금액을 차례대로 구한다.
 		for (int i = 1; i <= 2*C; i++) {
 			for (int j = 1; j <= N; j++) {
 				dp[i] = Math.min(dp[i], dp[Math.max(0, i-costs[j][1])] + costs[j][0]);
