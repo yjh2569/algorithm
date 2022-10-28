5d0f98aedf85731f7492b0acae32cfcc0a7f050d
yjh2569 <70595250+yjh2569@users.noreply.github.com>
2022-10-28 19:59:23 +0900

    BOJ 1240 노드사이의 거리
    * 트리의 특성에 의해 노드사이의 거리를 BFS를 통해 구할 수 있는 문제입니다.

---
+++ algorithm/src/graph/Main_1240_노드사이의_거리.java
@@ -0,0 +1,57 @@
+package graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main_1240_노드사이의_거리 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
+		for (int i = 0; i <= N; i++) {
+			graph.add(new ArrayList<>());
+		}
+		for (int i = 0; i < N-1; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			graph.get(a).add(new int[] {b, c});
+			graph.get(b).add(new int[] {a, c});
+		}
+		StringBuilder sb = new StringBuilder();
+		outer: for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			Queue<int[]> q = new LinkedList<>();
+			boolean[] visited = new boolean[N+1];
+			q.offer(new int[] {start, 0});
+			visited[start] = true;
+			while (!q.isEmpty()) {
+				int[] u = q.poll();
+				for (int[] v : graph.get(u[0])) {
+					if (!visited[v[0]]) {
+						if (v[0] == end) {
+							sb.append(u[1] + v[1]).append("\n");
+							continue outer;
+						}
+						visited[v[0]] = true;
+						q.offer(new int[] {v[0], u[1] + v[1]});
+					}
+				}
+			}
+		}
+		if (sb.length() > 0) sb.setLength(sb.length()-1);
+		System.out.println(sb.toString());
+	}
+
+}
